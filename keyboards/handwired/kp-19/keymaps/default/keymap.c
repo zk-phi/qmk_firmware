4e03c23a85b53f711bf6deb4e55e0a51349a2e7f
zk-phi <zerokay.phi@gmail.com>
2019-04-27 15:07:40 +0900

    tmp

---
+++ keyboards/handwired/kp-19/keymaps/default/keymap.c
@@ -0,0 +1,45 @@
+/* Copyright 2019 zk-phi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum layers {
+    DEFAULT,
+    VOLUP,
+    VOLDN,
+    AMPUP,
+    AMPDN
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [DEFAULT] = LAYOUT(
+        LT(VOLUP, KC_MEDIA_REWIND),
+        LT(VOLDN, KC_MEDIA_FAST_FORWARD),
+        LT(AMPUP, KC_MEDIA_PLAY_PAUSE),
+        LT(AMPDN, KC_MEDIA_PLAY_PAUSE)
+    ),
+    [VOLUP] = LAYOUT(
+        _______, KC__VOLUP, _______, _______
+    ),
+    [VOLDN] = LAYOUT(
+        KC__VOLDOWN, _______, _______, _______
+    ),
+    [AMPUP] = LAYOUT(
+        _______, _______, _______, _______
+    ),
+    [AMPDN] = LAYOUT(
+        _______, _______, _______, _______
+    )
+};
