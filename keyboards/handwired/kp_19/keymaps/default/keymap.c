678e18a23b0d2e29a059ac31e7f46cb55f532323
zk-phi <zerokay.phi@gmail.com>
2019-05-12 22:21:49 +0900

    Refactoring: move haptic-related code to haptic.c

--- keyboards/handwired/kp_19/keymaps/default/keymap.c
+++ keyboards/handwired/kp_19/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 
 #include "fft.c"
 #include "analog.h"
-#include "matrix.h"
+#include "haptic.c"
 
 enum layers {
     DEFAULT,
@@ -96,43 +96,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define max(A, B) ((A) > (B) ? (A) : (B))
 
 float amp = 1.0;
-int vib_duration = 40;
 
 LED_TYPE led[RGBLED_NUM];
-
-void haptic_task (void) {
-    static int vib_clear_timer = 0;
-    static matrix_row_t matrix_prev[MATRIX_ROWS];
-    matrix_row_t matrix_row = 0;
-    matrix_row_t matrix_change = 0;
-
-    if (vib_clear_timer) {
-        if ((int)(vib_duration - timer_elapsed(vib_clear_timer)) < 0) {
-            backlight_level(0);
-            vib_clear_timer = 0;
-        }
-    }
-
-    for (uint8_t r = 0; r < MATRIX_ROWS; r++) {
-        matrix_row = matrix_get_row(r);
-        matrix_change = matrix_row ^ matrix_prev[r];
-        if (matrix_change) {
-            for (uint8_t c = 0; c < MATRIX_COLS; c++) {
-                if (matrix_change & ((matrix_row_t)1<<c)) {
-                    if (matrix_row & ((matrix_row_t)1<<c)) {
-                        backlight_level(15);
-                        vib_clear_timer = timer_read();
-                    }
-                    matrix_prev[r] ^= ((matrix_row_t)1<<c);
-                }
-            }
-        }
-    }
-}
-
-void haptic_init (void) {
-    backlight_level(0);
-}
 
 void audio_illumination_task (void) {
     static bool power = false;
