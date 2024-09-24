6fdd5393856079e98c05789403c822f5e094750b
zk-phi <zk-phi@users.noreply.github.com>
2024-09-24 18:07:39 +0900

    Modify lain keymap

---
+++ keyboards/dm9records/lain/keymaps/via/config.h
@@ -0,0 +1,11 @@
+#define MOUSEKEY_DELAY             48
+#define MOUSEKEY_INTERVAL          24
+#define MOUSEKEY_MOVE_DELTA        3
+#define MOUSEKEY_MAX_SPEED         15 /* times faster than MOVE_DELTA */
+#define MOUSEKEY_TIME_TO_MAX       17
+
+#define MOUSEKEY_WHEEL_DELAY       72
+#define MOUSEKEY_WHEEL_INTERVAL    72
+#define MOUSEKEY_WHEEL_DELTA       1
+#define MOUSEKEY_WHEEL_MAX_SPEED   4 /* times faster */
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 255 /* disable auto-accel */
--- keyboards/dm9records/lain/keymaps/via/keymap.c
+++ keyboards/dm9records/lain/keymaps/via/keymap.c
@@ -5,20 +5,21 @@
 
 enum layers { BASE, NUM, FUNC, CONF, _04, _05 };
 
+/* WIP */
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
     [BASE] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_RBRC,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_INS, KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,
-        KC_LCTL,                   KC_LALT, KC_LGUI, FN_MO13, KC_SPC,    KC_ENT, FN_MO23, KC_RALT, KC_MENU, KC_RCTL,  KC_RSFT
+        KC_TAB,  KC_Q,    KC_W,    KC_E,         KC_R,   KC_T,    KC_Y,              KC_Y,            KC_U,     KC_I,    KC_O,    KC_P,     KC_MINS,
+        KC_LCTL, KC_A,    KC_S,    KC_D,         KC_F,   KC_G,    _______,           KC_H,            KC_J,     KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,         KC_V,   KC_B,             KC_B,     KC_N,            KC_M,     KC_COMM, KC_DOT,            KC_SLSH,
+        _______,          _______, LALT(KC_ESC), KC_SPC, KC_LANG2,         KC_LANG1, LT(NUM, KC_ENT), MO(FUNC), _______, _______,           _______
     ),
     [NUM] = LAYOUT(
-        KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,
-        KC_CAPS, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6),   S(KC_7), S(KC_8), S(KC_9), S(KC_0), S(KC_MINS),S(KC_EQL),
-        _______, _______, _______, _______, _______, _______,            KC_PSCR, _______, _______, _______, _______,  KC_BSLS,
-        _______,                   _______, _______, _______, _______,   _______, _______, _______, _______, _______,  _______
+        KC_JYEN, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+        _______, KC_TILD, _______, KC_LCBR, KC_LPRN, KC_LBRC, _______,          KC_RBRC, KC_RPRN, KC_RCBR, _______, KC_PIPE, KC_PLUS,
+        _______, _______, _______, _______, _______, _______,          KC_PSCR, _______, _______, _______, _______,          KC_BSLS,
+        _______,          _______, _______, _______, _______,          _______, _______, _______, _______, _______,          _______,
     ),
     [FUNC] = LAYOUT(
         KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
@@ -48,43 +49,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // clang-format on
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case USER00:
-            // via user keycode USER00 : toggle leds enable
-            if (record->event.pressed) {
-                lain_enable_leds_toggle();
-            }
-            return false;
-        default:
-            break;
-    }
-    return true;
-}
-
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-        case NUM:
-            lain_set_led(1, 1);
-            lain_set_led(2, 0);
-            break;
-        case FUNC:
-            lain_set_led(1, 0);
-            lain_set_led(2, 1);
-            break;
-        case CONF:
-            lain_set_led(1, 1);
-            lain_set_led(2, 1);
-            break;
-        default:
-            lain_set_led(1, 0);
-            lain_set_led(2, 0);
-            break;
+      case NUM:
+        lain_set_led(0, 0);
+        lain_set_led(1, 1);
+        lain_set_led(2, 0);
+        break;
+      case FUNC:
+        lain_set_led(0, 0);
+        lain_set_led(1, 0);
+        lain_set_led(2, 1);
+        break;
+      case CONF:
+        lain_set_led(0, 1);
+        lain_set_led(1, 1);
+        lain_set_led(2, 0);
+        break;
+      case _04:
+        lain_set_led(0, 1);
+        lain_set_led(1, 0);
+        lain_set_led(2, 1);
+        break;
+      case _05:
+        lain_set_led(0, 1);
+        lain_set_led(1, 1);
+        lain_set_led(2, 1);
+        break;
+      default:
+        lain_set_led(0, 1);
+        lain_set_led(1, 0);
+        lain_set_led(2, 0);
+        break;
     }
     return state;
 }
 
-bool led_update_user(led_t led_state) {
-    lain_set_led(0, led_state.caps_lock);
-    return false;
+void keyboard_post_init_user(void) {
+    lain_set_led(0, 1);
+    lain_set_led(1, 0);
+    lain_set_led(2, 0);
 }
